2223cca366dcf38ad7cee30cbab875c6867d3ba2
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-02-15 19:48:55 -0300

    feat: initial style config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,9 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return <main>asjidjais</main>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
